45ad688ec5788887930cd4d14b5cfa15edd2808f
Rkt019 <rajnikanttiwari019@gmail.com>
2026-09-24 17:32:47 +0530

    fifth commit

--- backend/main.py
+++ backend/main.py
@@ -58,7 +58,12 @@ SESSIONS: Dict[str, Dict] = {}
 app = FastAPI(title="Dance Assessment POC", version="2.0.0")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://olab-poc-nine.vercel.app"],
+    # allow_origins=["https://olab-poc-nine.vercel.app"],
+    allow_origins=[
+    "https://olab-poc.vercel.app",
+    "https://olab-poc-git-main-rk1019.vercel.app",
+    "http://localhost:3000",
+],
     allow_methods=["*"],
     allow_headers=["*"],
 )
